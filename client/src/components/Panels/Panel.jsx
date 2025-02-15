b0453a8ac0103346951158ca22f186038dccca98
Chand Ali <139934421+Chand-Ali-tech@users.noreply.github.com>
2025-02-16 00:21:52 +0500

    Update Panel.jsx

--- client/src/components/Panels/Panel.jsx
+++ client/src/components/Panels/Panel.jsx
@@ -17,7 +17,7 @@ function Panel() {
       try {
         setLoading(true);
         const res = await axios.get(
-          "http://localhost:8000/api/user/check-auth",
+          "https://tour-folio-backend.vercel.app/api/user/check-auth",
           {
             withCredentials: true,
           }
