5c69f2158e71b6ec24c61a7bc494c3535b1b0bf7
Chand Ali <139934421+Chand-Ali-tech@users.noreply.github.com>
2025-02-16 00:28:30 +0500

    Update SignUp.jsx

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -57,7 +57,7 @@ function SignUp() {
 
     try {
       setLoading(true);
-      const res = await axios.post("http://localhost:8000/api/user/signup", {
+      const res = await axios.post("https://tour-folio-backend.vercel.app/api/user/signup", {
         name,
         email,
         password,
@@ -93,7 +93,7 @@ function SignUp() {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8000/api/user/signup/verify-email",
+        "https://tour-folio-backend.vercel.app/api/user/signup/verify-email",
         {
           email,
           otp: code,
