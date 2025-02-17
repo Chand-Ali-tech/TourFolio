1bd294b8750cfee0cf910d1b1e16d86d0315d1f0
Moon Ali <chachand450@gmail.com>
2025-02-17 22:12:01 +0500

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,9 +23,9 @@ const CreateSendToken = async (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    // httpOnly: true,
+    // secure: true,
+    // sameSite: "strict",
   };
 
   res.cookie("jwt", token, cookieOptions);
