cc6334fb21260019670eef00511c5a124f87d104
Chand Ali <139934421+Chand-Ali-tech@users.noreply.github.com>
2025-02-17 19:32:49 +0500

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,7 +24,7 @@ const CreateSendToken = async (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "strict",
   };
 
